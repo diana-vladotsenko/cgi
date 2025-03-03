cfe0668d2882004dddd7f8801b6bf501be78effa
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-03-03 20:56:28 +0200

    added dependencies

---
+++ veebirakendus/src/main/java/ee/cgi/veebirakendus/VeebirakendusApplication.java
@@ -0,0 +1,13 @@
+package ee.cgi.veebirakendus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeebirakendusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeebirakendusApplication.class, args);
+	}
+
+}
---
+++ veebirakendus/src/test/java/ee/cgi/veebirakendus/VeebirakendusApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.cgi.veebirakendus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeebirakendusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
