4aba25b1c042955db3ce842925f1465bb3deea6b
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-03-11 13:47:03 +0200

    added search bar function, will try to make it better for UI

--- veebirakendus/frontend/components/FilterSection.jsx
+++ veebirakendus/frontend/components/FilterSection.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useState } from "react";
+
+
 
 const FilterSection = () => {
+  const [passengers, setPassengers] = useState(1);
+
   
+  function add(){
+    setPassengers(passengers + 1);
+  }
+  
+  function decrease(){
+    if (passengers > 1) {
+      setPassengers(passengers - 1);
+    }
+  }
   return (
     <div>
     <section>
@@ -9,26 +23,32 @@ const FilterSection = () => {
           <input className ="inputs-rounded" type="number" placeholder="Min"/>
           <input className ="inputs-rounded" type="number" placeholder="Max"/>
       </section>
-      <section>
+      {/* <section>
         <label for="Traveler">Traveler</label>
           <input  className ="inputs-rounded" type="number" placeholder=".."/>
-      </section>
+      </section> */}
 
       {/* passengers section */}
       <section>
         Passengers:
         <div id="numbers">
-          {/* by default */}
-          1
+          {/* by default is 1 */}
+          {passengers}
         </div>
-        <input type="button" onClick ="add()" value="+"/>
-        <input type="button" onClick ="decrease()" value="-"/>      
+        <input type="button" onClick ={add} value="+"/>
+        <input type="button" onClick ={decrease} value="-"/>      
         </section>
 
         <section>
-        Vali eelistusi:
-        <input type="checkbox" id="vehicle2" name="vehicle2" value="Car"/>
-        <label for="vehicle2"> I have a car</label>
+        Choose preferences:
+        <input type="checkbox" id="windowSeat" name="windowSeat" />
+        <label for="windowSeat"> Window seat</label>
+        <input type="checkbox" id="spaceForLegs" name="spaceForLegs" />
+        <label for="spaceForLegs"> More space for legs</label>
+        <input type="checkbox" id="closeToTheExit" name="closeToTheExit"/>
+        <label for="closeToTheExit"> Close to the exit</label>
+        <input type="checkbox" id="pairSeats" name="pairSeats"/>
+        <label for="pairSeats"> Pair seats</label>
       </section>
       <section>
         Cabin class:
--- veebirakendus/frontend/components/SearchBar.jsx
+++ veebirakendus/frontend/components/SearchBar.jsx
@@ -1,18 +1,75 @@
 import React from "react";
 import "../styles/globals.css";
+import { useState, useEffect } from "react";
+import AirportsList from "./AirportsList";  
+
 
 
 const SearchBar = () => {
+  const [airports, setAirports] = useState([]);
+  const [showAirportsList, setShowAirportsList] = useState(false);
+
+
+  useEffect(() =>{
+    const fetchAirports = async () => {
+      try {
+        const response = await fetch("/api/airports"); 
+        const data = await response.json();
+        console.log(data);
+        setAirports(data);
+      } catch (error) {
+        console.error("ERROR_AIRPORTS_ARE_NOT_FOUND");
+      }
+    };
+    fetchAirports();
+  }, []);
+
+  const toggleAirportsList = () => {
+    setShowAirportsList(true);
+    //console.log(airports);
+  }
+
+  //Chatgpt tegi seda:
+  const handleBlur = (e) => {
+    // Delay hiding the list to allow click events to register
+    //to delay the execution 0f the code inside for focus and blur
+    setTimeout(() => {
+      if (!document.activeElement.closest('.card-container')) {
+        setShowAirportsList(false);
+      }
+      //100 - milliseconds 
+    }, 100);
+  };
+
+  // const searchAirports = (e)=>{
+  //   //koodijupp w3 leheküljelt
+  //   let input, filter, a, i;
+  //   input = document.getElementById("search");
+  //   filter = input.value.toLowerCase();
+  //   //console.log(airports);
+
+  // // Loop through all list items, and hide those who don't match the search query
+  //   for (i = 0; i < airports.length; i++) {
+  //     a = airports[i].name;
+  //     //console.log(a);
+  //     if (a) {
+  //       a.style.display = "";
+  //     } else {
+  //       a.style.display = "Airports is not found..";
+  //     }
+  //   }
+  // }
+
   return (
-    <div className="card-container">
+    <div className="card-container" onBlur={handleBlur}>
       <search>
-          <div>
           <label>From</label>
-            <input name="searchBar" id="search" placeholder="From"/>
+            <input type = "text" name="searchBar" id="search" placeholder="From" onFocus={toggleAirportsList} onBlur={handleBlur} />
+            {/* onKeyUp={searchAirports} */}
           <label>To</label>
-          <input name="searchBar" id="search" placeholder="To"/>
-          </div>
-      </search>
+          <input type = "text" name="searchBar" id="search" placeholder="To" onFocus={toggleAirportsList } onBlur={handleBlur} />
+      </search>   
+    {showAirportsList && <AirportsList airports={airports} />};
   </div>
   );
 };
